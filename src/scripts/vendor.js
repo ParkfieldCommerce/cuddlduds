1e7f7277e72496fdf2798f44d07a70ee3a3cf981
Jibin M <jmathew1991@gmail.com>
2018-05-22 15:56:43 -0400

    Setting Up

--- src/scripts/vendor.js
+++ src/scripts/vendor.js
@@ -28,6 +28,13 @@
  */
 // =require vendor/Cartfox.js
 
+/* Swiper */
+// =require vendor/swiper.min.js
+
+/* Rivets/CartJS */
+// =require vendor/rivets-cart.js
+
+
 // Attempts to preserve comments that likely contain licensing information,
 // even if the comment does not have directives such as `@license` or `/*!`.
 //
