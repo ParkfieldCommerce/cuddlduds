4ea4912faf7f9ccfdf2150629d9bc0286789874e
Jibin M <jmathew1991@gmail.com>
2018-06-08 15:34:01 -0400

    more fb updates

--- src/scripts/custom.js
+++ src/scripts/custom.js
@@ -24,41 +24,39 @@ $(document).ready(function(){
     CartJS.addItem($(this).attr('data-variant-id'), 1);
   });
 
-  if($(window).width() > 600){//Only enable swatches for desktop
-    $('.js-QuickAddProductSwatch').click(function(){
-      var parentContainer = $(this).closest('.js-CollectionProductContainer');
-      var json = parentContainer.find('.js-CollectionProductJSON').html();
-      json = $.parseJSON(json);
-      var image = parentContainer.find('.js-CollectionProductImage');
-      var sizes = parentContainer.find('.js-CollectionProductSizes');
-      var selectedColor = $(this).attr('data-color');
-      var colorImageURLFound = false;
-      
-      //Sets Image and Size Variants
-      sizes.children('button').remove();
+  $('.js-QuickAddProductSwatch').click(function(){
+    var parentContainer = $(this).closest('.js-CollectionProductContainer');
+    var json = parentContainer.find('.js-CollectionProductJSON').html();
+    json = $.parseJSON(json);
+    var image = parentContainer.find('.js-CollectionProductImage');
+    var sizes = parentContainer.find('.js-CollectionProductSizes');
+    var selectedColor = $(this).attr('data-color');
+    var colorImageURLFound = false;
+    
+    //Sets Image and Size Variants
+    sizes.children('button').remove();
 
-      for(var i = 0; i < json.variants.length; i++){
-        var variant = json.variants[i];
-        if(variant.option1 == selectedColor && !colorImageURLFound){
-          colorImageURLFound = true;
-          if(variant.featured_image){
-            image.attr('src', variant.featured_image.src); 
-          }
+    for(var i = 0; i < json.variants.length; i++){
+      var variant = json.variants[i];
+      if(variant.option1 == selectedColor && !colorImageURLFound){
+        colorImageURLFound = true;
+        if(variant.featured_image){
+          image.attr('src', variant.featured_image.src); 
         }
-        if(variant.option1 == selectedColor && variant.available){
-          var size = '<button class="CollectionProduct__size-variant" type="button" data-variant-id="'+variant.id+'">'+variant.option2+'</button>';
-          sizes.append(size);
-        }
-      } 
+      }
+      if(variant.option1 == selectedColor && variant.available){
+        var size = '<button class="CollectionProduct__size-variant" type="button" data-variant-id="'+variant.id+'">'+variant.option2+'</button>';
+        sizes.append(size);
+      }
+    } 
 
-      //Initialize CartJS add event handler for created buttons
-      parentContainer.find('.js-CollectionProductSizes button').click(function(){
-        CartJS.addItem($(this).attr('data-variant-id'), 1);
-      });  
-
-      //Set Selected Button as Active
-      parentContainer.find('.Product__swatch').removeClass('Product__swatch--active');
-      $(this).addClass('Product__swatch--active');
+    //Initialize CartJS add event handler for created buttons
+    parentContainer.find('.js-CollectionProductSizes button').click(function(){
+      CartJS.addItem($(this).attr('data-variant-id'), 1);
     });  
-  }
+
+    //Set Selected Button as Active
+    parentContainer.find('.Product__swatch').removeClass('Product__swatch--active');
+    $(this).addClass('Product__swatch--active');
+  });  
 });
